fae5f0bb8dbb88983800c622d7cbd79a81c6ab17
nvaradha <nvaradha@iu.edu>
2023-02-19 17:31:40 -0500

    Added DB schema files

---
+++ DB_Schema/ex_insert_user_cred.sql
@@ -0,0 +1,40 @@
+INSERT INTO `easy_booking`.`user_cred`
+(
+`USERNAME`,
+`PASSWORD`,
+`USERTYPE`,
+`USERLOGIN_TYPE`,
+`EMAIL`,
+`ADDRESS`,
+`MOBILE_NUM`,
+`SEC_Q_1`,
+`SEC_Q_2`,
+`SEC_A_1`,
+`SEC_A_2`)
+VALUES
+(
+'testuser',
+'testpass',
+'customer',
+'native',
+'test@gmail.com',
+'1234 NW Bobcat Lane, St. Robert, MO 65584-5678' ,
+'5555551234',
+'What is you favorite test 1?',
+'test ans 1',
+'What is your favorite test 2?',
+'test and 2'),
+
+(
+'testuser2',
+'testpass',
+'customer',
+'native',
+'test2@gmail.com',
+'1234 NW Bobcat Lane, St. Robert, MO 65584-5678' ,
+'5555551235',
+'What is you favorite test 1?',
+'test ans 1',
+'What is your favorite test 2?',
+'test and 2');
+
---
+++ DB_Schema/ex_select_user_cred.sql
@@ -0,0 +1,13 @@
+SELECT * FROM USER_CRED;SELECT `user_cred`.`USER_ID`,
+    `user_cred`.`USERNAME`,
+    `user_cred`.`PASSWORD`,
+    `user_cred`.`USERTYPE`,
+    `user_cred`.`USERLOGIN_TYPE`,
+    `user_cred`.`EMAIL`,
+    `user_cred`.`ADDRESS`,
+    `user_cred`.`MOBILE_NUM`,
+    `user_cred`.`SEC_Q_1`,
+    `user_cred`.`SEC_Q_2`,
+    `user_cred`.`SEC_A_1`,
+    `user_cred`.`SEC_A_2`
+FROM `easy_booking`.`user_cred`;
