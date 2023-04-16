db79854d5d679bc3e3e82b6334a810937d37cbe1
Prabhu, Sagar Srinivas <saprabh@iu.edu>
2023-04-16 18:48:11 -0400

    Update HotelDatabase.sql

--- DB_Schema/HotelDatabase.sql
+++ DB_Schema/HotelDatabase.sql
@@ -43,3 +43,21 @@ create table IF NOT EXISTS booking(
 	FOREIGN KEY (user_id) REFERENCES users(id),
 	FOREIGN KEY (hotel_id) REFERENCES hotels(id)
 );
+
+CREATE TABLE `payments` (
+  `id` varchar(40) NOT NULL,
+  `checkout_id` varchar(80) NOT NULL,
+  `payment_method_id` varchar(40) NOT NULL,
+  `charge_id` varchar(40) NOT NULL,
+  `balance_transaction` varchar(40) NOT NULL,
+  `amount` float NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  `postal_code` varchar(15) NOT NULL,
+  `card` varchar(4) NOT NULL,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `payments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
